ba157bbfd727c7ce015e62d0a85e43ab2ff9adb4
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-09-27 10:04:39 +0300

    added delete and update functionality

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql2";
+import cors from "cors";
 
 const app = express();
 
@@ -11,6 +12,7 @@ const db = mysql.createConnection({
 });
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("hello");
@@ -26,12 +28,44 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`,`description`,`cover`) VALUES (?)";
-  const values = [req.body.title, req.body.description, req.body.cover];
+  const q =
+    "INSERT INTO books (`title`,`description`,`price`,`cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.cover,
+  ];
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json("Book has been created");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted");
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title`=?,`description`=?,`price`=?,`cover`=? WHERE id= ?";
+
+  const values = [
+    req.body.title,
+    req.body.description,
+    req.body.price,
+    req.body.cover,
+  ];
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated");
   });
 });
 
